b0c3fb85e573394db874876f8c5854658345dce0
Federico Moretta <65861902+fmoretz@users.noreply.github.com>
2020-12-15 19:44:56 +0100

    Project Finished
    if any improvemetns can be done, you are free to do

--- Encrypting-Decrypting/file_encrypting.cpp
+++ Encrypting-Decrypting/file_encrypting.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <fstream>
 #include <stdio.h>
+#include <cstdio>
 #define key 100
 
 using namespace std;
@@ -13,82 +14,107 @@ int main(){
 	char script[100], ch;
 	fstream ftin, ftout;
 	int todo;
+	bool loop = true;
 
-	// Menù Selection
-	cout << "Choose an action:" << endl,
-	cout << "1. Encrypt file." << endl;
-	cout << "2. Decrypt file." << endl;
-	cout << "3. Exit." << endl;
+	while(loop){
+		// Menù Selection
+		cout << "Choose an action:" << endl,
+		cout << "1. Encrypt file." << endl;
+		cout << "2. Decrypt file." << endl;
+		cout << "3. Exit." << endl;
+		cout << " --> ";
+		
+		cin >> todo;
 
-	cin >> todo;
+		switch(todo) {
+			case 1 :
 
-	switch(todo) {
-		case 1 :
+				//Encrypting Section
+				//call the file 
+				cout << "Enter the file name:" <<endl;
+				cin >> script;
 
-			//Encrypting Section
-			//call the file 
-			cout << "Enter the file name:" <<endl;
-			cin >> script;
+				ftin.open(script, fstream::in);
 
-			ftin.open(script, fstream::in);
+				ftout.open("tmp.txt", fstream::out);
 
-			ftout.open("tmp.txt", fstream::out);
+				// Raise error
+				if(!ftin){cout << "Error --> No file: " << script << " is present." << endl;}
+				if(!ftout){cout << "Error --> Internal issue, enter the source to fix "<< endl;}
 
-			// encrypt message loop
-			while(ftin>>noskipws>>ch){
-				
-				// encrypting key
-				ch = ch + key;
-				ftout << ch;
+
+				// encrypt message loop
+				while(ftin>>noskipws>>ch){
+					
+					// encrypting key
+					ch = ch + key;
+					ftout << ch;
+				}
+
+				ftout.close();
+				ftin.close();
+
+				//copyng in the original file the encrypt code
+				ftin.open(script, fstream::out);
+				ftout.open("tmp.txt", fstream::in);
+
+				// Raise error
+				if(!ftin){cout << "Error --> Issue during encrypting, enter the source to fix" << endl;}
+				if(!ftout){cout << "Error --> Internal issue, enter the source to fix "<< endl;}
+
+				while(ftout>>noskipws>>ch){
+					ftin << ch;
+				}
+
+				ftin.close();
+				ftout.close();
+
+				cout << "\nFile encrypted successfully\n" << endl;
+				//remove('tmp.txt');
+				break;
+
+			case 2 :
+
+			    //Decrypt Section
+				//call the file 
+				cout << "Enter the file name:" <<endl;
+				cin >> script;
+
+				ftin.open(script, fstream::out);
+
+				ftout.open("tmp.txt", fstream::in);
+
+				// Raise error
+				if(!ftin){cout << "Error --> No file: " << script << " is present." << endl;}
+				if(!ftout){cout << "Error --> Internal issue, enter the source to fix "<< endl;}
+
+				// encrypt message loop
+				while(ftout>>noskipws>>ch){
+					
+					// encrypting key
+					ch = ch - key;
+					ftin << ch;
+				}
+
+				ftout.close();
+				ftin.close();
+
+				// Raise error
+				if(!ftin){cout << "Error --> Issue during encrypting, enter the source to fix" << endl;}
+				if(!ftout){cout << "Error --> Internal issue, enter the source to fix "<< endl;}
+
+				cout << "\nFile decrypted successfully\n" << endl;
+				break;
+
+			case 3 :
+				loop=false;
+				break;
+
+			default:
+				cout << "Invalid choice. please select one of the listed\n." <<endl;
 			}
-
-			ftout.close();
-			ftin.close();
-
-			//copyng in the original file the encrypt code
-			ftin.open(script, fstream::out);
-			ftout.open("tmp.txt", fstream::in);
-
-			while(ftout>>noskipws>>ch){
-				ftin << ch;
-			}
-
-			ftin.close();
-			ftout.close();
-
-			cout << "\nFile encrypted successfully" << endl;
-			//remove('tmp.txt');
-			break;
-
-		case 2 :
-
-		    //Decrypt Section
-			//call the file 
-			cout << "Enter the file name:" <<endl;
-			cin >> script;
-
-			ftin.open(script, fstream::out);
-
-			ftout.open("tmp.txt", fstream::in);
-
-			// encrypt message loop
-			while(ftout>>noskipws>>ch){
-				
-				// encrypting key
-				ch = ch - key;
-				ftin << ch;
-			}
-
-			ftout.close();
-			ftin.close();
-
-			cout << "\nFile decrypted successfully" << endl;
-			break;
-
-		case 3 :
-
-			break;
 	}
 
+	remove("tmp.txt");
 	return 0;
 }
